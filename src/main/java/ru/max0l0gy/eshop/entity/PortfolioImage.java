7596172323cfbdb12a083bfff63691ab8a503f9e
max0l0gy <maxmorev@gmail.com>
2021-04-28 21:18:54 +0300

    Fixed PortfolioImage entity

--- src/main/java/ru/max0l0gy/eshop/entity/PortfolioImage.java
+++ src/main/java/ru/max0l0gy/eshop/entity/PortfolioImage.java
@@ -3,6 +3,8 @@ package ru.max0l0gy.eshop.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import javax.persistence.Column;
@@ -13,8 +15,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Accessors(chain = true)
 @Table(name = "portfolio_image")
@@ -38,5 +42,18 @@ public class PortfolioImage implements Comparable<PortfolioImage> {
     @Override
     public int compareTo(PortfolioImage commodityImage) {
         return Short.compare(getImageOrder(), commodityImage.getImageOrder());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PortfolioImage that = (PortfolioImage) o;
+        return Objects.equals(getId(), that.getId()) && getUri().equals(that.getUri()) && getPortfolio().equals(that.getPortfolio()) && getImageOrder().equals(that.getImageOrder());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUri(), getPortfolio(), getImageOrder());
     }
 }
